2f786b081447b1d4579712efc84165ebbb4190a0
lucaspere <lucasrupestre@hotmail.com>
2020-06-28 14:22:02 -0300

    adding the getProduct function into api

--- server/api.js
+++ server/api.js
@@ -1,13 +1,14 @@
 const Products = require('./products')
 
 module.exports = {
-   listProducts
+   listProducts,
+   getProduct
 }
 
 
-async function listProducts(req, res) {
+async function listProducts (req, res) {
    res.setHeader('Access-Control-Allow-Origin', '*')
-   const {offset = 0, limit = 25, tag} = req.query
+   const { offset = 0, limit = 25, tag } = req.query
 
    try {
       res.json(await Products.list({
@@ -17,5 +18,19 @@ async function listProducts(req, res) {
       }))
    } catch (err) {
       return res.status(500).json({ error: err.message })
+   }
+}
+
+async function getProduct (req, res, next) {
+   res.setHeader('Acess-Control-Allow-Origin', '*')
+   const { id } = req.params
+
+   try {
+      const products = await Products.get(id)
+      if(!products) return next()
+
+      res.json(products)
+   } catch (err) {
+      res.status(500).json({ error: err.message})
    }
 }
