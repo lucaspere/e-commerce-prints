0bd37997e4417c814efd4fd556ed21a7ac35f99b
lucaspere <lucasrupestre@hotmail.com>
2020-06-28 14:22:24 -0300

    adding the get function into products.js

--- server/products.js
+++ server/products.js
@@ -4,13 +4,20 @@ const path = require('path')
 const productsFile = path.join(__dirname, '../products.json')
 
 module.exports = {
-   list
+   list,
+   get
 }
 
-async function list (opts = {}) {
-   const {offset = 0, limit = 25, tag} = opts
+async function list(opts = {}) {
+   const { offset = 0, limit = 25, tag } = opts
    const data = await fs.readFile(productsFile)
    return JSON.parse(data)
       .filter((p, i) => !tag || p.tags.indexOf(tag) >= 0)
       .slice(offset, offset + limit)
+}
+
+async function get (id = '') {
+   const products = JSON.parse(await fs.readFile(productsFile))
+
+   return products.find((item) => item._id === id)
 }
