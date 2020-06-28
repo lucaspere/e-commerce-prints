8b144c23e0b83d368866d5e680986c72dd33a400
lucaspere <lucasrupestre@hotmail.com>
2020-06-28 13:53:07 -0300

    adding the api

---
+++ server/api.js
@@ -0,0 +1,21 @@
+const Products = require('./products')
+
+module.exports = {
+   listProducts
+}
+
+
+async function listProducts(req, res) {
+   res.setHeader('Access-Control-Allow-Origin', '*')
+   const {offset = 0, limit = 25, tag} = req.query
+
+   try {
+      res.json(await Products.list({
+         offset: Number(offset),
+         limit: Number(limit),
+         tag
+      }))
+   } catch (err) {
+      return res.status(500).json({ error: err.message })
+   }
+}
